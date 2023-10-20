b8438d9959c0e2e12e78d663a018ed0ab0964cc3
Michle99 <sammiyonyx@gmail.com>
2023-10-20 10:01:02 -0400

    Methods Revisited answered

--- src/index.js
+++ src/index.js
@@ -96,3 +96,121 @@ for (let i of ninja_turtle) {
 }
 
 document.getElementById("ninja_new_name").innerHTML = ninja_names;
+
+
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+let method_results = "";
+
+const favMovies = [
+  "Jaws",
+  "The Fellowship of the Ring",
+  "Howl's Moving Castle",
+  "Django Unchained",
+  "Cloud Atlas",
+  "The Usual Suspects",
+  "Toy Story",
+  "Conan the Barbarian",
+  "Titanic",
+  "Harry Potter",
+  "Fried Green Tomatoes",
+  "Volver",
+  "Oculus",
+  "Seven",
+  "Black Panther",
+  "Harry Potter",
+  "Imitation of Life",
+  "Snatch",
+  "Fast and Furious"
+];
+
+// Index of Titanic
+method_results = favMovies.indexOf("Titanic"); //
+document.getElementById("method_revisited").innerHTML =
+  "Index of Titanic: " + method_results;
+
+// 1
+let sorted_favMovies = favMovies.sort();
+document.getElementById("sorted_movies").innerHTML =
+  "<h5>Sorted favMovies array w/o params: </h5>" + sorted_favMovies;
+
+// 2. The array indices were rearranged, not altered
+
+// 3. Use the method pop
+let popped_index = "";
+popped_index = favMovies.pop();
+console.log("popped index:", popped_index);
+document.getElementById("popped_movies").innerHTML =
+  "<p>Value of popped index: </p>" + popped_index;
+
+// 4. push "Guardians of the Galaxy"
+favMovies.push("Guardians of the Galaxy");
+document.getElementById("pushed_movie").innerHTML =
+  "<h5>Added Guardian of the Galaxy to array: </h5>" + favMovies;
+
+// 5. Reverse the array.
+let reversed_array = "";
+reversed_array = favMovies.reverse();
+document.getElementById("reverse_array").innerHTML =
+  "<h5>Reversed the favMovies array: </h5>" + reversed_array;
+
+// 6. Use the shift method.
+let shifted_array = "";
+shifted_array = favMovies.shift();
+document.getElementById("shift_array").innerHTML =
+  "<h5>Value of the favMovies array that got shifted: </h5>" + shifted_array;
+
+// 7. unshift - what does it return? It returns the array length
+let unshifted_array = "";
+unshifted_array = favMovies.unshift();
+document.getElementById("unshift_array").innerHTML =
+  "<h5>Result of the unshift method: </h5>" + unshifted_array;
+
+// 8.splice "Django Unchained" and add "Avatar"
+//Find the index of "Django Unchained"
+let spliced_array = "";
+const djangoIndx = favMovies.indexOf("Django Unchained");
+console.log("Index of django:", djangoIndx);
+spliced_array = favMovies.splice(djangoIndx, 1, "Avatar");
+console.log("Spliced Django Unchained: ", spliced_array);
+document.getElementById("splice_array").innerHTML = spliced_array;
+console.log("Add Avatar: ", favMovies);
+
+// 9.Did this permanently alter our array?
+// Array was permanently altered: The value @ index 14 was removed and replaced by "Avatar"
+let sliced_array = "";
+const middleIndex = Math.floor(favMovies.length / 2);
+sliced_array = favMovies.slice(middleIndex);
+document.getElementById("slice_array").innerHTML = sliced_array;
+
+// 10. slice the last half of the array
+// Programmatically determine the mid array
+let midIndex = Math.floor(favMovies.length / 2);
+console.log("Slicing array: ", favMovies.slice(midIndex));
+
+// 11. yes, a new array was returned than the original array.
+
+// 12. Store the value of your slice in a variable and console.log it.
+let slicedd_array = favMovies.slice(midIndex);
+console.log("Sliced array: ", slicedd_array);
+
+// 13. hought Question: What is going on here? A new array was returned.
+
+/// 14. console.log your final results
+console.log("Final results: ", sliced_array);
+
+// Index of Fast and Furious
+console.log(
+  "Fast and Furious index: ",
+  sliced_array.indexOf("Fast and Furious")
+);
+
+/*** Fast and Furious is present in both arrays: sliced and favMovies arrays */
+// Index of Fast and Furious
+let fast_furious_index = favMovies.indexOf("Fast and Furious");
+document.getElementById("fast_furious_ind").innerHTML = fast_furious_index;
+console.log("Movie name: ", favMovies[13]);
+
+
