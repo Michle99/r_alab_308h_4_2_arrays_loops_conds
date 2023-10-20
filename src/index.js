4e9bfe4cca6328be6ee13b859ebdbdedb1b3821c
Michle99 <sammiyonyx@gmail.com>
2023-10-20 09:59:01 -0400

    Wild Wild Life answered

--- src/index.js
+++ src/index.js
@@ -43,3 +43,41 @@ function fizzbuzz() {
 
 fizzbuzz();
 
+
+
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+
+// 1
+let wild = "";
+wild += plantee[2] + 1;
+
+document.getElementById("plantee_age").innerHTML = "Plantee age is: " + wild;
+
+// 2
+let wolfy_new_town = "";
+wolfy[3] = "Gotham City";
+wolfy_new_town += wolfy[3];
+document.getElementById("wolfy_hometown").innerHTML =
+  "Wolfy's hometown is: " + wolfy_new_town;
+
+// 3
+dart.push("Hawkins");
+console.log("New dart array after adding Hawkins: ", dart);
+
+document.getElementById("dart_new_town").innerHTML =
+  "Added a second Hometown: " + dart;
+
+// 4
+let wolfy_new_name = "";
+wolfy[0] = "Gameboy";
+wolfy_new_name = wolfy[0];
+
+document.getElementById("wolfy_new_name").innerHTML =
+  "Wolfy new name: " + wolfy_new_name;
